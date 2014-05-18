46754a3c003a80a25540b942b953c08087953959
Mike Patella <m.c.patella@gmail.com>
2014-05-18 17:34:43 -0400

    change console msg

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.use(express.static('public'));
 app.get('/', express.static('public/index.html'));
 
 app.listen(app.get('port'), function() {
-  console.log('Phaser is now served on port ' + app.get('port'));
+  console.log('Quest Quest is now available on port ' + app.get('port'));
 });
